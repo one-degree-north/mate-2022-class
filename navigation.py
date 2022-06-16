5fedd9a03ce873fdad255f3d7f8919d939d2eff3
ayambabu23 <72435742+ayambabu23@users.noreply.github.com>
2022-06-16 15:12:42 +0800

    Calculate time needed to move forward
    Very basic physics

---
+++ navigation.py
@@ -0,0 +1,18 @@
+import time
+
+def time_delay(distance, accel):
+    # d = 0.5at^2
+    delay = sqrt(d*2.0/a)
+    return delay
+    
+
+def move_forward(distance: float):
+    # Kevin: Start thrusters to go forward
+    length_of_packet = 9
+    baud_rate = 115200 # or whatever it is
+    delay = (length_of_packet + 2) / baud_rate
+    time.sleep(delay)
+    # Kevin/Alan: Get BNO Acceleration
+    delay = time_delay(distance, accel)
+    time.sleep(delay)
+    # Kevin: Stop thrusters from going forward
