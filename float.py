d68c5953ab3cda17db45b1d3ee98768ccee237b8
GoldSpaghetti <alan.chen.2k6@gmail.com>
2022-06-17 15:24:35 +0800

    a

--- float.py
+++ float.py
@@ -5,10 +5,12 @@ BUFSIZE = 4096
 HEADER = b'0xAB'
 FOOTER = b'0xB3'
 
+def readData():
+    pass
+
 def main():
     sock = socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((ESP_LOCAL_IP, PORT))
-
 
 if __name__ == "__main__":
     main()
