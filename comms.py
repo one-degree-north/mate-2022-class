d9ec1152dd83b27bdbef817e0f2752c06e577ece
GoldSpaghetti <alan.chen.2k6@gmail.com>
2022-06-19 10:15:45 +0800

    comms update

--- comms.py
+++ comms.py
@@ -21,14 +21,23 @@ class AccelData:
 class Comms:    #COMMENTING THINGS OUT FOR TEST ON LAPTOP
     def __init__(self, controls=None, outputQueue=None):
         ports = list_ports.comports()
-        # offshorePort = "/dev/cu.usbmodem14201"
-        onshorePort = "/dev/cu.usbserial-1120"
+        offshorePort = "/dev/cu.usbmodem142101"
+        onshorePort = "/dev/cu.usbserial-142101"
         for port in ports:
-           if port.description == "USB Serial":
-               onshorePort = port.device
-           #elif port.description == "FT232R USB UART - FT232R USB UART":
-           #    offshorePort = port.device
-
+            print(f"product: {port.product}")
+            print(f"device: {port.device}")
+            if port.product == "QT Py M0":
+                print(f"QT Py found")
+                offshorePort = port.device
+            #elif port.description == "FT232R USB UART - FT232R USB UART":
+            #    print(f"serial connection found")
+            #    offshorePort = port.device
+            if port.product == "USB Serial":
+                print(f"arduino nano found")
+                onshorePort = port.device
+            #elif port.description == "USB Serial":
+            #    onshorePort = port.device
+        
         # self.offshoreArduino = Serial(port=f"{offshorePort}", baudrate=115200)
         self.onshoreArduino = Serial(port=f"{onshorePort}", baudrate=115200)
         self.thrusterPins = [0, 1, 2, 3, 4, 5]  #maps thruster position via index to pins. [midL, midR, frontL, frontR, backL, backR]
